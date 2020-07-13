7bb4fe2b7e53a4bd7c19db1e52e00037977d167b
Melroy Neil Dsouza <meldsza@gmail.com>
2020-07-13 16:59:44 +0530

    Update Faculty.php

--- app/Faculty.php
+++ app/Faculty.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Faculty extends Model
 {
-    protected $fillable = ['employee_code', 'designation', 'test'];
+    protected $fillable = ['employee_code', 'designation', 'test', 'test2'];
     public function user()
     {
         return $this->morphOne('App\User', 'details');
